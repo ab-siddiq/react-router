f3d62890e09f59f268e401a76b43e6880ae61b37
siddiq <siddiq.aiub@gmail.com>
2022-04-12 18:21:31 +0600

    added footer component

---
+++ src/Footer/Footer.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Footer = () => {
+    return (
+        <div className='bg-green-900 text-green-700 '>
+            <p>copyright { new Date().getFullYear}</p>
+            <p>All right reserve Abu Bakkar Siddiq</p>
+        </div>
+    );
+};
+
+export default Footer;
